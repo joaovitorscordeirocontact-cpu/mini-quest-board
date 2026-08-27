a1b13cc40790c9106303edd2fd145951d00340a9
joaovitorcordeiro-dev <joaovitorscordeiro.contact@gmail.com>
2026-08-27 10:15:39 -0300

    feat(Dashboard): Create SummaryCards component to show quests data

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -51,11 +51,61 @@ function PageHeading({ page }) {
   );
 }
 
+function SummaryCards() {
+  return (
+    <div className="summary-cards-block">
+      <div className="total-quests-card">
+        <div className="card-image">
+          <img alt="clipboard image" src="#"/>
+        </div>
+        <div className="card-info">
+          <span>Total Quests</span>
+          <span>0</span>
+        </div>
+      </div>
+
+      <div className="completed-quests-card">
+        <div className="card-image">
+          <img alt="clipboard image" src="#"/>
+        </div>
+        <div className="card-info">
+          <span>Completed</span>
+          <span>0</span>
+        </div>
+      </div>
+
+      <div className="pending-quests-card">
+        <div className="card-image">
+          <img alt="clipboard image" src="#"/>
+        </div>
+        <div className="card-info">
+          <span>Pending</span>
+          <span>0</span>
+        </div>
+      </div>
+
+      <div className="progress-card">
+        <div className="card-image">
+          <img alt="clipboard image" src="#"/>
+        </div>
+        <div className="card-info">
+          <span>Progress</span>
+          <span>0</span>
+        </div>
+        <div className="progress-bar"></div>
+      </div>
+    </div>
+  );
+}
+
+
+
 export function Dashboard(){
   return (
     <>
       <Header/>
       <PageHeading page={currentPage[0]}/>
+      <SummaryCards/>
     </>
   );
 }
