b7f8f0cfd332fce82ff91b44e10301aa3b57a64a
joaovitorcordeiro-dev <joaovitorscordeiro.contact@gmail.com>
2026-08-28 10:26:23 -0300

    feat(dashboard): make the view quests button interactive

--- src/pages/dashboard-page/Dashboard.jsx
+++ src/pages/dashboard-page/Dashboard.jsx
@@ -4,10 +4,11 @@ import { PageHeading } from "../../components/PageHeading";
 import { SummaryCards } from "./SummaryCards";
 import { QuestOfTheDay } from "./QuestOfTheDay";
 import { RecentQuests } from "./RecentQuests";
+import { useNavigate } from "react-router-dom";
 
 
 export function Dashboard({page, quests, completeQuest, deleteQuest }) {
-  
+  const navigate = useNavigate();
 
   return (
     <>
@@ -17,7 +18,7 @@ export function Dashboard({page, quests, completeQuest, deleteQuest }) {
       <QuestOfTheDay quests={quests} completeQuest={completeQuest} deleteQuest={deleteQuest} />
       <RecentQuests quests={quests} completeQuest={completeQuest} deleteQuest={deleteQuest} />
 
-      <button className="view-quests-btn">
+      <button className="view-quests-btn" onClick={() => navigate('/Quests')}>
         View all quests
       </button>
     </>
